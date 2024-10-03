58a568a33c7f55e5b8c16264c367737fbdc05560
David Antonio <happywybie@gmail.com>
2024-10-03 10:12:42 -0600

    ci: add a blue database connection string

--- obj/Debug/net6.0/dw-api-web.AssemblyInfo.cs
+++ obj/Debug/net6.0/dw-api-web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dw-api-web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f880b6cba6ecdc3f7321e8a452fde0ea851f585")]
 [assembly: System.Reflection.AssemblyProductAttribute("dw-api-web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dw-api-web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
